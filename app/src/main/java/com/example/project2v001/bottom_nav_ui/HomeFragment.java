d74af1576cca5cfee7dd548dafebaa70cb1a77c8
Ahmad Jaber <30752881+BionikMlc@users.noreply.github.com>
2020-04-01 00:36:13 +0300

    another bunch of fixes

--- app/src/main/java/com/example/project2v001/bottom_nav_ui/HomeFragment.java
+++ app/src/main/java/com/example/project2v001/bottom_nav_ui/HomeFragment.java
@@ -58,56 +58,57 @@ public class HomeFragment extends Fragment {
 
             postListView.addOnScrollListener(new RecyclerView.OnScrollListener() {
                 @Override
-                public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                    super.onScrollStateChanged(recyclerView, newState);
-                    boolean reachedBottom = !recyclerView.canScrollVertically(1);
-                    if (reachedBottom) {
+                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    boolean isReachedBottom =  !recyclerView.canScrollVertically(1);
+
+                    if(isReachedBottom){
                         loadPosts();
                     }
                 }
             });
 
-            Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(2);
+            Query firstQuery = firebaseFirestore.collection("Posts")
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .limit(8);
             firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
                 @Override
                 public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                    if (e == null) {
+                        if (!documentSnapshots.isEmpty()) {
+                            if (isFirstDataLoad) {
+                                lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                                postsList.clear();
+                            }
 
-                    if (!documentSnapshots.isEmpty()) {
+                            for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
 
-                        if (isFirstDataLoad) {
+                                if (doc.getType() == DocumentChange.Type.ADDED) {
 
-                            lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
-                            postsList.clear();
+                                    String postId = doc.getDocument().getId();
+                                    Post post = doc.getDocument().toObject(Post.class).withId(postId);
 
-                        }
+                                    if (isFirstDataLoad) {
 
-                        for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                                        postsList.add(post);
 
-                            if (doc.getType() == DocumentChange.Type.ADDED) {
+                                    } else {
 
-                                String blogPostId = doc.getDocument().getId();
-                                Post blogPost = doc.getDocument().toObject(Post.class).withId(blogPostId);
+                                        postsList.add(0, post);
 
-                                if (isFirstDataLoad) {
+                                    }
 
-                                    postsList.add(blogPost);
 
-                                } else {
-
-                                    postsList.add(0, blogPost);
+                                    postAdapter.notifyDataSetChanged();
 
                                 }
-
-
-                                postAdapter.notifyDataSetChanged();
-
                             }
+
+                            isFirstDataLoad = false;
+
                         }
 
-                        isFirstDataLoad = false;
-
                     }
-
                 }
 
             });
@@ -122,31 +123,32 @@ public class HomeFragment extends Fragment {
             Query nextQuery = firebaseFirestore.collection("Posts")
                     .orderBy("timestamp", Query.Direction.DESCENDING)
                     .startAfter(lastVisible)
-                    .limit(2);
+                    .limit(8);
 
             nextQuery.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
                 @Override
                 public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                    if (e == null) {
+                        if (!documentSnapshots.isEmpty()) {
+                            lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                            //                        postsList.clear();
+                            for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
 
-                    if (!documentSnapshots.isEmpty()) {
-                        lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
-                        for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+                                if (doc.getType() == DocumentChange.Type.ADDED) {
 
-                            if (doc.getType() == DocumentChange.Type.ADDED) {
+                                    String blogPostId = doc.getDocument().getId();
+                                    Post blogPost = doc.getDocument().toObject(Post.class).withId(blogPostId);
+                                    postsList.add(blogPost);
 
-                                String blogPostId = doc.getDocument().getId();
-                                Post blogPost = doc.getDocument().toObject(Post.class).withId(blogPostId);
-                                postsList.add(blogPost);
+                                    postAdapter.notifyDataSetChanged();
+                                }
 
-                                postAdapter.notifyDataSetChanged();
                             }
-
                         }
+
                     }
-
                 }
-            });
-
+                });
         }
     }
 }
